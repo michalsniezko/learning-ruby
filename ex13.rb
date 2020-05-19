53a98f912b83fc3723712c11c5e5205a3ea41700
Michał Śnieżko <michalsniezko@gmail.com>
2020-05-19 20:03:28 +0200

    ex13, 14 and readme

---
+++ ex13.rb
@@ -0,0 +1,8 @@
+first, second, third = ARGV
+
+print "Now give me some input: "
+input = $stdin.gets.chomp
+puts "Your first variable is #{first}"
+puts "Your second variable is #{second}"
+puts "Your third variable is #{third}"
+puts "And the input you gave me is #{input}"
